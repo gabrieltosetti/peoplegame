2be45a17c05b7b2b4b5a047be7e8ca64f60fbea1
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-06-20 14:37:36 -0300

    Site - Tentativa 1

---
+++ app/MyApp.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App;
+
+class MyApp  extends \Illuminate\Foundation\Application
+{
+    public function publicPath()
+   {
+     return $this->basePath.DIRECTORY_SEPARATOR.'/public_html';
+     }
+}
